f0b6447f05524994158607a052acca9b0918ac26
Jose_Luis <joselu2004.jlrc@gmail.com>
2023-11-24 09:43:16 +0100

    vista

---
+++ src/main/java/com/example/cronometro/App.java
@@ -0,0 +1,38 @@
+package com.example.cronometro;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    private static Scene scene;
+
+    // Método para cambiar la raíz de la escena
+    public static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        // Cargar la escena
+        scene = new Scene(loadFXML("cronometro"), 800, 600);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    // Método para cargar un archivo FXML
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/cronometro/CronoController.java
@@ -0,0 +1,28 @@
+package com.example.cronometro;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Circle;
+
+public class CronoController {
+        @FXML
+        private Button btn_init;
+
+        @FXML
+        private Button btn_lap;
+
+        @FXML
+        private Button btn_pause;
+
+        @FXML
+        private Button btn_reset;
+
+        @FXML
+        private Circle pelota;
+
+        @FXML
+        private Label timer;
+
+
+}
